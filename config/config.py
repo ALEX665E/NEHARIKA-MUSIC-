230af7d64d598cc0612efb5a5b090093fc5097d4
Piyush Malviya <115225926+CoderXPiyush@users.noreply.github.com>
2023-04-29 14:06:50 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -168,7 +168,7 @@ autoclean = []
 
 
 # Images
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/91b047d3a5523abc22283.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/4557a895210e6cc768d63.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
